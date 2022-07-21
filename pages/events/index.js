626145e155e94c4fc91f39390264197bac3d5e59
alexandre <akekiwy@gmail.com>
2022-07-21 15:35:29 +1000

    slug filtered event jsx and styling

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,17 @@
+import { Fragment } from 'react';
+
 import { getAllEvents } from '../../dummy-data';
 import EventList from '../../components/events/events-list';
+import EventSearch from '../../components/events/event-search';
 
 const AllEvents = () => {
   const events = getAllEvents();
 
   return (
-    <div>
+    <Fragment>
+      <EventSearch />
       <EventList items={events} />
-    </div>
+    </Fragment>
   );
 };
 
