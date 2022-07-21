15243f62c49ad4db6af67f11efc6518e6b274c1c
alexandre <akekiwy@gmail.com>
2022-07-21 14:45:05 +1000

    All event page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,5 +1,14 @@
+import { getAllEvents } from '../../dummy-data';
+import EventList from '../../components/events/events-list';
+
 const AllEvents = () => {
-  return <div> All Events</div>;
+  const events = getAllEvents();
+
+  return (
+    <div>
+      <EventList items={events} />
+    </div>
+  );
 };
 
 export default AllEvents;
