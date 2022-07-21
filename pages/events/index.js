a8ea313a9ad3ee0b4e398dcd22e8db7ddda945b8
alexandre <akekiwy@gmail.com>
2022-07-21 15:55:07 +1000

    slug filtered event logic and dynamic route

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,4 +1,5 @@
 import { Fragment } from 'react';
+import { useRouter } from 'next/router';
 
 import { getAllEvents } from '../../dummy-data';
 import EventList from '../../components/events/events-list';
@@ -6,10 +7,16 @@ import EventSearch from '../../components/events/event-search';
 
 const AllEvents = () => {
   const events = getAllEvents();
+  const router = useRouter();
+  function findEventsHandler(year, month) {
+    //construction the full path (destination) with template literal
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
 
   return (
     <Fragment>
-      <EventSearch />
+      <EventSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </Fragment>
   );
