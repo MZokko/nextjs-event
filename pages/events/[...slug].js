90fdd557174f15a31e178e390a37124aeac9ac8e
alexandre <akekiwy@gmail.com>
2022-07-21 16:21:04 +1000

    loading proper event in filtered event  errors

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,6 +1,40 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import { getFilteredEvents } from '../../dummy-data';
 
 const FilteredEventPage = () => {
+  const router = useRouter();
+
+  const filteredData = router.query.slug;
+  // console.log('filtData : ' + filteredData);
+
+  //avoiding error because we dont have access to the data YET
+  if (!filteredData) {
+    return <p className='center'> loading...</p>;
+  }
+  //second render data are there , extract them into variables
+  const filteredYear = filteredData[0];
+  const filteredMonth = filteredData[1];
+  // converting year and month into the right format NUMBER
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+  //error handler
+  if (
+    isNan(numYear) ||
+    isNan(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p> invalid filter. Try with new values</p>;
+  }
+  //get the event corresponding to the selected month and year
+  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
+  //check if filteredEvent is 'falsy' or empty array
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p> No event find for the chosen filter.</p>;
+  }
+
   return (
     <div>
       <h1>FilteredEventPage</h1>
